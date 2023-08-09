257fceda42b5947156b38aa45b0db375de1077bb
Kamlendu Shekhar <kashekha@kashekha.remote.csb>
2023-08-08 20:57:17 -0400

    creat vms

--- templates/ocp_vm_creator.j2.sh
+++ templates/ocp_vm_creator.j2.sh
@@ -9,7 +9,7 @@ CMD="virt-install -n bootstrap.ocp4.example.com --description Bootstrap_Machine_
      --network bridge=openshift4,mac=52:54:00:a4:db:5f"
 {% endif %}
 {% if vm_type == 'master01' %}
-CMD="virt-install -n master02.ocp4.example.com --description Master1_MachineforOpenshift4Cluster \
+CMD="virt-install -n master01.ocp4.example.com --description Master1_MachineforOpenshift4Cluster \
      --os-type=Linux --os-variant=rhel8.0 --ram=4096 --vcpus=4 \
      --autostart --noreboot \
      --disk pool=default,bus=virtio,size=50 \
